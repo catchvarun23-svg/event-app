71405082c01b8a15f732bbace93189955cddfb86
catchvarun23-svg <catchvarun23@gmail.com>
2026-03-23 22:35:43 +1100

    Update event.js

--- api/event.js
+++ api/event.js
@@ -1,6 +1,5 @@
 export default async function handler(req, res) {
-  const url = "https://script.google.com/macros/s/AKfycbzJ8NPxiQX1RhSxkVyQu8QeuE0mTA-V_A-M4aL07b0wm2HXbqwAW8LocfO5k1bwwmH9/exec?action=event";
-
+  const url = "https://script.googleusercontent.com/macros/echo?user_content_key=AWDtjMWx78g2JV4NMCIsmQtpUUlYD86bCbjzN-dd_1-QCMFrL2OfqnPrSqH_om71cOCurcsTLRVz0hKRXElhE60sbro0rggpoKUB4Nl8dhCECUwqd4axew_9qrH7e5FUJVslRTNZdHc9MFh50nJNJCYcQFKP48Pub58R-p49viohOVf48VzQ5HOwuUbMpJrmQhYXhSRykKyGCj1QYNBhoPQYkF0FAVGt2NzDS9cOLS6UAoSz54IJjThombBbtk_YDXxx3o6z6oO1w5_K1AIpLvOys-ls9ZP9CjLZbUnjVCbyED5PmH6e0kc&lib=M62DVCIDDVDbIk9zHKt2ODOA3qwdUz_ea&action=event";
   try {
     const response = await fetch(url, {
       method: "GET",
